cbbe31d73d8d9bbfa7e5b38fee97b4bee80468bd
Gaurav Bhandari <sbhandari1907@protonmail.com>
2023-01-10 12:19:41 +0530

    added /anime command

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,7 @@ const TOKEN = process.env.TOKEN;
 if (!TOKEN) throw new Error("Token not found");
 
 const bot = new telegramBot(TOKEN, { polling: true })
-
-
-
-
 // chatbotapi
-
 bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, "Hello World")
 })
@@ -23,63 +18,40 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
     bot.sendMessage(chatId, resp);
 })
 
-bot.onText(/\/photo/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendPhoto(chatId, "https://picsum.photos/200/300", {
-        caption: "I'm a bot!"
-    });
-}
-
-);
-
-
-
-bot.onText(/\/location/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendLocation(chatId, 12.233, 44.2112);
-});
-
-bot.onText(/\/venue/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendVenue(chatId, 50.4501, 30.5234, "Tbilisi", "Georgia");
-});
-
-bot.onText(/\/contact/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendContact(chatId, "+995 599 99 99 99", "John Doe");
-});
-
 bot.onText(/\/dice/, (msg) => {
     const chatId = msg.chat.id;
     bot.sendDice(chatId);
 }
 );
 
-bot.onText(/\/game/, (msg) => {
+// "&is_nsfw=false"
 
+bot.onText(/\/anime/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendGame(chatId, "game");
-});
-
-bot.onText(/\/inline/, (msg) => {
-
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, "Inline keyboard", {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: "Google",
-                        url: "https://google.com"
-                    }
-                ]
-            ]
+    async function getWaifu() {
+        const response = await fetch("https://api.waifu.im/search/?is_nsfw=false");
+        const data = await response.json();
+        console.log(data.images[0].url);
+        const url = data.images[0].preview_url;
+        try {
+            bot.sendPhoto(chatId, url, {
+                reply_to_message_id: msg.message_id,
+                reply_markup: {
+                    inline_keyboard: [[
+                        {
+                            text: "Download",
+                            url: data.images[0].url
+                        }
+                    ]
+                    ]
+                }
+            });
+        } catch (e) {
+            bot.sendMessage(chatId, "Error");
         }
-    });
-}
-);
-
-
+    }
+    getWaifu();
+})
 
 
 
@@ -89,12 +61,15 @@ bot.on("message", (msg) => {
     const chatId: number = msg.chat.id;
     const greetings = ['hi', 'hlo', 'Hello', 'HLO', 'Hi', 'Hii', 'hello'];
     if (!msg.text) return;
-    if (greetings.includes(msg.text) && msg.from?.id === admin) {
-        bot.sendMessage(msg.chat.id, "Hello Boss", {
-            reply_to_message_id: msg.message_id
-        })
-    } else
-        bot.sendMessage(chatId, `Hello ${msg.chat.first_name} @${msg.chat.username}`);
+    if (greetings.includes(msg.text)) {
+        if (msg.from?.id === admin) {
+            bot.sendMessage(msg.chat.id, "Hello Boss", {
+                reply_to_message_id: msg.message_id
+            })
+        } else
+            bot.sendMessage(chatId, `Hello ${msg.chat.first_name} @${msg.chat.username}`);
+
+    }
 
 })
 
